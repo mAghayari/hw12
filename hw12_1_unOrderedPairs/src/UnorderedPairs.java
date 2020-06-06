b367915d4fa7ad6df045fc146a2c28bc83ab59a7
aghayari <you@example.com>
2020-06-06 17:07:42 +0430

    enhancing the sort method performance

--- hw12_1_unOrderedPairs/src/UnorderedPairs.java
+++ hw12_1_unOrderedPairs/src/UnorderedPairs.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class UnorderedPairs {
     public ArrayList<Integer> sort(ArrayList<Integer> numbers) {
@@ -8,32 +7,12 @@ public class UnorderedPairs {
         else if (numbers.size() % 2 != 0)
             numbers.remove(numbers.size() - 1);
 
-        ArrayList<Integer> evenIndexes = new ArrayList<>();
-        ArrayList<Integer> oddIndexes = new ArrayList<>();
-        ArrayList<Integer> newList = new ArrayList<>();
-
-        for (int i = 0; i < numbers.size(); i += 2) {
-            evenIndexes.add(numbers.get(i));
-            oddIndexes.add(numbers.get(i + 1));
-        }
-
-        Iterator evenIterator = evenIndexes.iterator();
-        Iterator oddIterator = oddIndexes.iterator();
-        int oddIndex;
-        int evenIndex;
-
-        while (evenIterator.hasNext()) {
-            evenIndex = (int) evenIterator.next();
-            oddIndex = (int) oddIterator.next();
-            if (evenIndex > oddIndex) {
-                evenIterator.remove();
-                oddIterator.remove();
+        for (int i = 0; i < numbers.size() - 1; i += 2) {
+            if (numbers.get(i) > numbers.get(i + 1)) {
+                numbers.remove(numbers.get(i));
+                numbers.remove(numbers.get(i));
             }
         }
-        for (int i = 0; i < evenIndexes.size(); i++) {
-            newList.add(evenIndexes.get(i));
-            newList.add(oddIndexes.get(i));
-        }
-        return newList;
+        return numbers;
     }
 }
